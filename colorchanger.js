47e49f639cac46dee9d813e84b839624d4d50be5
Chukwu Emmanuel Eke <chukwuemmanueleke123@gmail.com>
2026-05-02 19:18:28 +0100

    Add color changer functionality with buttons

---
+++ colorchanger.js
@@ -0,0 +1,65 @@
+let container = document.querySelector('.container');
+let background = document.querySelector('#background');
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let button3 = document.querySelector('#button3');
+let button4 = document.querySelector('#button4');
+let button5 = document.querySelector('#button5');
+let button6 = document.querySelector('#button6');
+let button7 = document.querySelector('#button7');
+let button8 = document.querySelector('#button8');
+let button9 = document.querySelector('#button9');
+let button10 = document.querySelector('#button10');
+let button11 = document.querySelector('#button11');
+let button12 = document.querySelector('#button12');
+let button13 = document.querySelector('#button13');
+let button14 = document.querySelector('#button14');
+let button15 = document.querySelector('#button15');
+
+
+
+button1.addEventListener('click',function(){
+document.body.style.backgroundColor='red';
+});
+button2.addEventListener('click',function(){
+document.body.style.backgroundColor='orange';
+});
+button3.addEventListener('click',function(){
+document.body.style.backgroundColor='yellow';
+});
+button4.addEventListener('click',function(){
+document.body.style.backgroundColor='purple';
+});
+button5.addEventListener('click',function(){
+document.body.style.backgroundColor='black';
+});
+button6.addEventListener('click',function(){
+document.body.style.backgroundColor='green';
+});
+button7.addEventListener('click',function(){
+document.body.style.backgroundColor='cyan';
+});
+button8.addEventListener('click',function(){
+document.body.style.backgroundColor='crimson';
+});
+button9.addEventListener('click',function(){
+document.body.style.backgroundColor='salmon';
+});
+button10.addEventListener('click',function(){
+document.body.style.backgroundColor='tomato';
+});
+button11.addEventListener('click',function(){
+document.body.style.backgroundColor='teal';
+});
+button12.addEventListener('click',function(){
+document.body.style.backgroundColor='gold';
+});
+button13.addEventListener('click',function(){
+document.body.style.backgroundColor='orchid';
+});
+button14.addEventListener('click',function(){
+document.body.style.backgroundColor='darkslategrey';
+});
+button15.addEventListener('click',function(){
+document.body.style.backgroundColor='white';
+});
